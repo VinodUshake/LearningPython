d4a0d88cc8577fbd1044ffb3f86e443bb2ebfdac
Vinod Ushake <vinod.ushake@gmail.com>
2022-10-04 00:18:11 +1100

    initial version of Sub program

---
+++ add_functional.py
@@ -0,0 +1,9 @@
+#step 1 get the first number
+def add(x,y):
+    #step 3 add two numbers & store results
+    z = x + y
+    #step# 4 print
+    print(z)
+    x = 20
+    # Step 2 get second number
+    y = 30
