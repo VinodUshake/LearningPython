a49801192da9f7e388ff8c83bc14e9e1e714970b
Vinod Ushake <vinod.ushake@gmail.com>
2022-10-26 09:32:33 +1100

    Slicing_String

---
+++ String_ Slicing.py
@@ -0,0 +1,14 @@
+#Slicing = Create a substring by  by extracting elements from another string
+# indexing [] or Slice()
+# Slicing funtion there are 3 arguments, depending on where and how to slice string
+# Staring index, stoping index and step [Start:stop:step]
+# let's being with Starting Index
+# lets say we have string
+
+name = "vinod Ushaker"
+#create a substring based on slice portion  " name")
+
+first_name = name[0]
+# we need starting and stoping index.  where do want to being our slice, it's always starts with 0
+print(first_name)
+
